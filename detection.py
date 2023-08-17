09245281840a96aeaf8f353377f609ab21dd9961
Ryan O'Callaghan <ryan5591@outlook.com>
2023-08-17 15:48:38 -0400

    Updated detection.py

--- detection.py
+++ detection.py
@@ -10,7 +10,7 @@ picture_taken = st.camera_input("Take a pic")
 if picture_taken:
     uploaded_image = PIL.Image.open(picture_taken)
 
-    with st.spinner("Detecting images..."):
+    with st.spinner("Detecting objects..."):
         model = YOLO("yolov8n.pt")
         res = model.predict(uploaded_image)
 
